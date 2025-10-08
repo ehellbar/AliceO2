0d19e459a84caec5d121f53b684fda6b7772ad48
Ernst Hellbar <ernst.hellbar@cern.ch>
2025-10-08 14:16:26 +0200

    DPL: add command line option to print error instead of warning when exit transition timer expires

--- Framework/Core/include/Framework/DeviceContext.h
+++ Framework/Core/include/Framework/DeviceContext.h
@@ -33,6 +33,7 @@ struct DeviceContext {
   int expectedRegionCallbacks = 0;
   int exitTransitionTimeout = 0;
   int dataProcessingTimeout = 0;
+  bool errorOnExitTransitionTimeout = false;
 };
 
 } // namespace o2::framework
