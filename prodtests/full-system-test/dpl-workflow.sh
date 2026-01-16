becd1c77e6ca5f21d75ee8c4d02854bbc29991e2
Ernst Hellbar <ernst.hellbar@cern.ch>
2026-01-16 13:19:51 +0100

    dpl-workflow.sh: enable ALPIDE_ERR_DUMPS by default in online physics runs

--- prodtests/full-system-test/dpl-workflow.sh
+++ prodtests/full-system-test/dpl-workflow.sh
@@ -227,6 +227,8 @@ if [[ $EPNSYNCMODE == 1 ]]; then
   # dump raw data in case of GPU crash and set dump directory size limits; files are automatically cleaned by EPN after 60 days
   GPU_CONFIG_KEY+="GPU_proc.debugOnFailure=1;GPU_proc.debugOnFailureDirectory=/data/tf/debug;GPU_proc.debugOnFailureMaxFiles=1000;GPU_proc.debugOnFailureMaxSize=500;GPU_proc.debugOnFailureSignalMask=2240;"
 
+  [[ $RUNTYPE == "PHYSICS" ]] && ALPIDE_ERR_DUMPS=1
+
   EVE_OPT+=" --eve-dds-collection-index 0"
   MIDDEC_CONFIG+=" --feeId-config-file \"$MID_FEEID_MAP\""
   if [[ $EXTINPUT == 1 ]] && [[ $GPUTYPE != "CPU" ]] && [[ -z "$GPU_NUM_MEM_REG_CALLBACKS" ]]; then
